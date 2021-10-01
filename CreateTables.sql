9ff6ce5297a30a996a6bf3f04a9b9e5060ea92d3
Arya Varaste <30382009+varaste@users.noreply.github.com>
2021-10-01 12:22:56 +0330

    Add files via upload

---
+++ CreateTables.sql
@@ -0,0 +1,30 @@
+﻿/*drop table "Reserves";
+drop table "Sailors";
+drop table "Boats";
+*/
+CREATE TABLE "Sailors"
+(
+  sid INTEGER NOT NULL,
+  sname CHARACTER VARYING(30),
+  age INTEGER,
+  rating INTEGER,
+  PRIMARY KEY (sid )
+);
+
+CREATE TABLE "Boats"
+(
+  bid INTEGER NOT NULL,
+  color CHARACTER VARYING(10),
+  bname CHARACTER VARYING(10),
+  PRIMARY KEY (bid )
+);
+
+CREATE TABLE "Reserves"
+(
+  sid INTEGER NOT NULL,
+  bid INTEGER NOT NULL,
+  date DATE NOT NULL,
+  PRIMARY KEY (sid , bid , date ),
+  FOREIGN KEY (bid) REFERENCES "Boats" (bid),
+  FOREIGN KEY (sid) REFERENCES "Sailors" (sid)
+);
